418caa0a76ac3d63cff8c76a978a8612fe7da0f3
LSM <dltmdals6718@naver.com>
2023-08-15 12:27:46 +0900

    @RequestParam -> @ModelAttribute 1. @ModelAttribute 사용시 Form의 name으로 setXXX, getXXX 를 찾아 객체의 필드에 바인딩한다. 2. @ModelAttribute가 model에 자동으로 담아주는 클래스명으로 담는다.

--- src/main/java/hello/springmvc/web/basic/BasicItemController.java
+++ src/main/java/hello/springmvc/web/basic/BasicItemController.java
@@ -6,10 +6,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -38,10 +35,45 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
-    @PostMapping("add")
-    public String save() {
-        return "basic/addForm";
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+        itemRepository.save(item);
+        model.addAttribute("item", item);
+        return "basic/item";
     }
+
+    //@PostMapping("add")
+    public String addItemV2(@ModelAttribute("item") Item item,
+                            Model model) {
+        itemRepository.save(item);
+        //model.addAttribute("item", item); 이 과정을 생략 가능하다.
+        // @ModelAttribute(name="item")으로 name을 지정한 값으로 모델에 담긴다.
+        return "basic/item";
+    }
+
+    //@PostMapping("add")
+    public String addItemV3(@ModelAttribute Item itemName) {
+        itemRepository.save(itemName);
+//        name 속성이 생략되면
+//        클래스명의 첫 글자를 소문자로 바꿔 모델이 담는다.
+//                HelloData -> helloData로 모델이 담김.
+//                주의! 파라미터명이 아닌 클래스명으로 모델에 담는다.
+        return "basic/item";
+    }
+
+    @PostMapping("add")
+    public String addItemV4(Item itemName) {
+        itemRepository.save(itemName);
+//        @ModelAttribute 생략도 가능하다. 객체의 경우 @ModelAttribute가, 단순 타입인 경우 @RequestParam 적용
+        return "basic/item";
+    }
+
     @PostConstruct
     public void init() {
         itemRepository.save(new Item("itemA", 10000, 10));
