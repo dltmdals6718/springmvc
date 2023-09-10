888c17a49fda6d695cfcb4b22a320471c827068c
LSM <dltmdals6718@naver.com>
2023-09-10 13:34:45 +0900

    Bean Validation - 필드 적용

--- src/main/java/hello/springmvc/validation/item/Item.java
+++ src/main/java/hello/springmvc/validation/item/Item.java
@@ -1,13 +1,24 @@
 package hello.springmvc.validation.item;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
 
 @Data
 public class Item {
 
     private Long id;
+    @NotBlank(message = "공백X")
     private String itemName;
+
+    @NotNull
+    @Range(min=1000, max=100000)
     private Integer price;
+
+    @NotNull
+    @Max(99)
     private Integer quantity;
 
     public Item() {
