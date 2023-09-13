3aba70e44aadb70a48d1cf94bc78bba74f9fad2c
LSM <dltmdals6718@naver.com>
2023-09-13 13:26:38 +0900

    Bean Validation - 수정 페이지에도 적용 전역 에러 같은 경우 자바 코드로 처리해주자.

--- src/main/java/hello/springmvc/validation/web/validation/ValidationItemControllerV3.java
+++ src/main/java/hello/springmvc/validation/web/validation/ValidationItemControllerV3.java
@@ -44,6 +44,13 @@ public class ValidationItemControllerV3 {
     @PostMapping("/add")
     public String addItem(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
+        if(item.getPrice()!= null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice<10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+
         //검증에 실패하면 다시 입력 폼으로
         if(bindingResult.hasErrors()) {
             //model.addAttribute("errors", bindingResult);
@@ -65,7 +72,21 @@ public class ValidationItemControllerV3 {
     }
 
     @PostMapping("/{itemId}/edit")
-    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+    public String edit(@PathVariable Long itemId, @Validated @ModelAttribute Item item, BindingResult bindingResult) {
+
+        if(item.getPrice()!= null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice<10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+
+        //검증에 실패하면 다시 입력 폼으로
+        if(bindingResult.hasErrors()) {
+            //model.addAttribute("errors", bindingResult);
+            return "validation/v3/editForm";
+        }
+
         itemRepository.update(itemId, item);
         return "redirect:/validation/v3/items/{itemId}";
     }
