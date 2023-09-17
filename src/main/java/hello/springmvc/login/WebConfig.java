9fdf67af06006914080b19ab7da21664e537c696
LSM <dltmdals6718@naver.com>
2023-09-17 14:54:51 +0900

    인터셉터 - 모든 요청 로그 남기기

--- src/main/java/hello/springmvc/login/WebConfig.java
+++ src/main/java/hello/springmvc/login/WebConfig.java
@@ -2,27 +2,30 @@ package hello.springmvc.login;
 
 import hello.springmvc.login.web.filter.LogFilter;
 import hello.springmvc.login.web.filter.LoginCheckFilter;
+import hello.springmvc.login.web.intercepter.LogIntercepter;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
-public class WebConfig {
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogIntercepter())
+                .order(1)
+                .addPathPatterns("/**") // 서블릿과 달리 **이라고 해줘야함 - 하위는 전부다라는 뜻
+                .excludePathPatterns("/css/**", "*.ico", "/error");
+    }
+
     @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
         filterRegistrationBean.setOrder(1);
-        filterRegistrationBean.addUrlPatterns("/*");
-        return filterRegistrationBean;
-    }
-
-    @Bean
-    public FilterRegistrationBean loginCheckFilter() {
-        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
-        filterRegistrationBean.setFilter(new LoginCheckFilter());
-        filterRegistrationBean.setOrder(2);
         filterRegistrationBean.addUrlPatterns("/*");
         return filterRegistrationBean;
     }
