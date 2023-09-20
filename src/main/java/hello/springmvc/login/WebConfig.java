db43fbf718e5944efd0c856ad0cffb459aa46f60
LSM <dltmdals6718@naver.com>
2023-09-20 15:00:05 +0900

    스프링 인터셉터 - 인증 체크

--- src/main/java/hello/springmvc/login/WebConfig.java
+++ src/main/java/hello/springmvc/login/WebConfig.java
@@ -1,14 +1,19 @@
 package hello.springmvc.login;
 
+import hello.springmvc.login.web.argumentresolver.LoginMemberArgumentResolver;
 import hello.springmvc.login.web.filter.LogFilter;
 import hello.springmvc.login.web.filter.LoginCheckFilter;
 import hello.springmvc.login.web.intercepter.LogIntercepter;
+import hello.springmvc.login.web.intercepter.LoginCheckIntercepter;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -19,9 +24,15 @@ public class WebConfig implements WebMvcConfigurer {
                 .order(1)
                 .addPathPatterns("/**") // 서블릿과 달리 **이라고 해줘야함 - 하위는 전부다라는 뜻
                 .excludePathPatterns("/css/**", "*.ico", "/error");
+
+        registry.addInterceptor(new LoginCheckIntercepter())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/members/add", "/login",
+                        "/logout", "/css/**", "/*.ico", "/error");
     }
 
-    @Bean
+    //@Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
